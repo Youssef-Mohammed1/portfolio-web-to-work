a83b0d70b705390420d9a5945621f04196661aa7
Youssef Mohammed <yousygy7@gmail.com>
2026-05-05 01:34:54 +0300

    edit gallary section in project page

--- project page/script.js
+++ project page/script.js
@@ -85,3 +85,25 @@ navLinks.forEach(link => {
     menuToggle.classList.remove("open");
   });
 });
+
+// Swiper js
+ new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+
+});
